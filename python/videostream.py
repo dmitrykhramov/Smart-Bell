d0db43e74c747247f3e0c41de6dbd4eaa1287d29
Soeun <thdmsdia@gmail.com>
2017-07-27 10:34:02 -0200

    save 15 images in a folder named last id and face detection

--- python/videostream.py
+++ python/videostream.py
@@ -4,11 +4,9 @@ import time
 from threading import Thread
 import RPi.GPIO as GPIO
 from datetime import datetime
-import oscd ..
+import os
 import collect
-from makephoto import MakePhotos
-
-#makephotos_thread = None
+from pymongo import MongoClient
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -18,13 +16,16 @@ class Stream(Thread):
 		self.flag = [True]
 		self.capture_flag = [False]
 		self.ws = ws
-		#self.flodername = 1
+		self.foldername = []
 		Thread.__init__(self, name=Stream.__name__)
 		
 	def run(self):
 		self.camera = cv2.VideoCapture(0)
 		
 		prev_input = 1
+		
+		client = MongoClient('localhost',27017)
+		db = client.smartbell.visitors		
 		
 		while self.flag[0]:
 			rval, frame = self.camera.read()
@@ -47,18 +48,28 @@ class Stream(Thread):
 			time.sleep(0.05)
 			
 			if self.capture_flag[0] == True:
-				path = 'pics/' + str(datetime.now()) #str(self.foldername[0])
+				#Get last _id from mongodb
+				db_data = db.find().sort('_id',1)
+				for cur in db_data:
+					self.foldername = cur['_id']
+				
+				path = 'pics/' + str(self.foldername)
 				if not os.path.exists(path):
 					os.makedirs(path)
+				
 				count = 0
+				s_time = time.time()
 				while count < 15:
+					
 					file_name = '/img' + str(datetime.now()) + '.jpg'
 					success, capture_img = self.camera.read()
 					small_frame = cv2.resize(capture_img, (0, 0), fx=0.25, fy=0.25)
 					if collect.collect_pictures(capture_img, path, file_name):
 						count += 1
 						print("finish")
+					
 					time.sleep(1)
+				print(time.time()-s_time)
 				self.capture_flag[0] = False
 		self.camera.release()
 	
@@ -67,53 +78,4 @@ class Stream(Thread):
 
 	def stop(self):
 		self.flag[0] = False
-		
-	'''	
-class Stream(Thread):
-	def __init__(self, ws):
-		self.flag = [True]
-		self.capture_flag = [False]
-		self.ws = ws
-		#self.flodername = 1
-		Thread.__init__(self, name=Stream.__name__)
-		
-	def run(self):
-		self.camera = cv2.VideoCapture(0)
-		
-		prev_input = 1
-		
-		while self.flag[0]:
-			rval, frame = self.camera.read()
-			
-			if frame is None:
-				self.camera.release()
-				self.camera = cv2.VideoCapture(0)
-				continue
-			
-			rvel, jpeg = cv2.imencode('.jpg', frame)
-			encode_string = base64.b64encode(jpeg)
-			self.ws.write_message(encode_string)
-			'''
-			but = GPIO.input(17)
-			
-			if(not prev_input and but):
-				print(but)
-			
-			prev_input = but
-			'''
-			time.sleep(0.05)
-			
-			if self.capture_flag[0] == True:
-				global makephotos_thread
-				makephotos_thread = MakePhotos(self)
-				makephotos_thread.start()
-				self.capture_flag[0] = False
-				
-		self.camera.release()
 	
-	def change_flag(self):
-		self.capture_flag[0] = True
-
-	def stop(self):
-		self.flag[0] = False
-'''
