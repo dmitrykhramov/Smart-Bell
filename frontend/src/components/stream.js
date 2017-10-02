7b90d030c8d9e6773ba6f68a2c96602d2753db87
Soeun <thdmsdia@gmail.com>
2017-10-02 05:03:52 -0200

    working version 4

--- frontend/src/components/stream.js
+++ frontend/src/components/stream.js
@@ -26,13 +26,6 @@ class Stream extends Component {
             } else if (msg.data == 'success' || msg.data == 'fail') {
                 this.props.photoMake(msg.data);
             }  else {
-                //var decoding = atob(msg.data); // decode a string of data from python which has been encoded using base64 encoding
-                //var len = decoding.length;
-                //var buffer = new ArrayBuffer(len);
-                //var bytes = new Uint8Array(buffer);
-                //for (var i = 0; i < len; i++)
-                //    bytes[i] = decoding.charCodeAt(i);
-                //var url = encode(bytes);
                 var url = 'data:image/jpg;base64,'+msg.data
                 this.setState({url: url});
             }
@@ -58,33 +51,3 @@ class Stream extends Component {
 }
 
 export default connect(null, actions)(Stream);
-
-// Converts image bytes from the server and returns image url
-function encode(input) {
-    var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    var output = "";
-    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-    var i = 0;
-
-    while (i < input.length) {
-        chr1 = input[i++];
-        chr2 = i < input.length ? input[i++] : Number.NaN; // Not sure if the index
-        chr3 = i < input.length ? input[i++] : Number.NaN; // checks are needed here
-
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
-        }
-
-        output += keyStr.charAt(enc1) + keyStr.charAt(enc2) +
-            keyStr.charAt(enc3) + keyStr.charAt(enc4);
-    }
-
-    return "data:image/jpg;base64," + output;
-}
